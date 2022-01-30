aa85593156dd1178a684e468d3a82b6b1e518b7d
randomgeekdom <randomgeekdom@gmail.com>
2022-01-30 14:11:52 -0600

    Sample Custom Moves

--- src/app/pages/sample-custom-moves/sample-custom-moves.component.ts
+++ src/app/pages/sample-custom-moves/sample-custom-moves.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import reference from 'reference.json';
 
 @Component({
   selector: 'app-sample-custom-moves',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleCustomMovesComponent implements OnInit {
 
-  constructor() { }
+  public moves = [];
+  public searchTerm = "";
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.moves = reference.Sample_Custom_Moves.sort((one, two) => (one.Name > two.Name ? 1 : -1));
   }
+
+
+  get sortedItems(){
+    return this.moves.filter(x=>!this.searchTerm ||
+      x.Name.toLowerCase().includes(this.searchTerm) ||
+      x.Description.toLowerCase().includes(this.searchTerm) )
+  }
+
 
 }
