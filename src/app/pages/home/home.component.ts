41f8d9fd82e39d6e3c5048e89749b4f14e297f71
randomgeekdom <randomgeekdom@gmail.com>
2022-01-29 18:49:06 -0600

    Updating home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public updates: string[]=[
-"hello",
-"goodbye"
-  ];
+    public updates: string[] = [
+        '2022-01-29: A new year, a new version of the website has been released.'
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-
-  }
-
-
-
-
-
+    ngOnInit(): void {}
 }
